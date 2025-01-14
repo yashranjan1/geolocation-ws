5bc863e6a00467f8c4c9d5ca3d5cdf5b0729ecf4
Yash <yash.ranjan25@gmail.com>
2025-01-14 16:34:24 +0530

    added room based message sending

--- server.js
+++ server.js
@@ -5,20 +5,36 @@ const { join } = require('node:path');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => { 
+
+  socket.on('init', (data) => {
+    console.log('User connected');
+    socket.join(data.room);
+  });
+  
+  socket.on('send message', (data) => {
+    console.log('rec received:', data);
+    console.log(socket.rooms);
+    io.to(data.room).emit('rec message', data.message); 
+  });  
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
 });
 
 
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log('Server is running on port 3000');
 });
